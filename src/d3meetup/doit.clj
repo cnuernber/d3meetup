c730778d6f2c4950d0c12944dcc7e8181ddf1ce8
Chris Nuernberger <chris@thinktopic.com>
2019-03-10 15:46:31 -0600

    Have some graphs and stuff.

--- src/d3meetup/doit.clj
+++ src/d3meetup/doit.clj
@@ -6,7 +6,8 @@
             [tech.compute.tensor.functional :as tens-fun]
             [clojure.core.matrix :as m]
             [clojure.set :as c-set]
-            [oz.core :as oz])
+            [oz.core :as oz]
+            [clojure.pprint :as pp])
   (:import [java.time Duration]))
 
 
@@ -51,8 +52,11 @@
      (Math/pow 2 31)))
 
 
+(def lat-lon [:position-lat :position-long] )
+
+
 (def load-pipeline
-  '[[m= [:position-lat :position-long] (* (col) 8.381903171539307E-8)]])
+  [['m= lat-lon '(* (col) 8.381903171539307E-8)]])
 
 
 (defn run-pipeline
@@ -64,7 +68,73 @@
 (defonce test-ds (fit-file->dataset fit/test-fname))
 
 
-(def processed-ds (run-pipeline test-ds))
+(def processed-pipeline (run-pipeline test-ds))
+
+(def processed-ds (:dataset processed-pipeline))
+
+(def lat-lon-data (-> (ds/select processed-ds (concat lat-lon
+                                                                 [:timestamp]) :all)
+                      (ds/->flyweight)))
+
+(def effort-speed-height-time (-> (ds/select processed-ds
+                                             [:timestamp :altitude :power :speed
+                                              :cadence]
+                                             :all)
+                                  (ds/->flyweight)))
+
+(def timestamp-data (ds-col/stats (ds/column processed-ds :timestamp)
+                                  [:min :max]))
+
+(def altitude-data (ds-col/stats (ds/column processed-ds
+                                            :altitude)
+                                 [:min :max]))
+
+(defn duration->str
+  [^Duration dur]
+  (.toString dur))
+
+(def view-ds [:div
+              [:h2 (format "Behold - %s - %s"
+                           (ds/dataset-name processed-ds)
+                           (duration->str (ds-duration processed-ds)))]
+              [:h3 "Geo Data"]
+              [:vega-lite {:data {:values lat-lon-data}
+                           :width 800
+                           :height 600
+                           :projection {:type :albersUsa}
+                           :mark :circle
+                           :encoding {:latitude {:field (first lat-lon)
+                                                 :type :quantitative}
+                                      :longitude {:field (second lat-lon)
+                                                  :type :quantitative}}}]
+              [:h3 "Graphs"]
+              [:div
+               [:div
+                [:vega-lite {:repeat {:row [:altitude]}
+                             :spec {:data {:values effort-speed-height-time}
+                                    :width 800
+                                    :mark :point
+                                    :encoding {:x {:field :timestamp
+                                                   :type :quantitative
+                                                   :scale {:domain
+                                                           [(:min timestamp-data)
+                                                            (:max timestamp-data)]}}
+                                               :y {:field :altitude
+                                                   :type :quantitative
+                                                   :scale {:domain [(:min altitude-data)
+                                                                    (:max altitude-data)]}}}}}]]
+               [:div
+                [:vega-lite {:repeat {:row [:power :speed :cadence]}
+                             :spec {:data {:values effort-speed-height-time}
+                                    :width 800
+                                    :mark :point
+                                    :encoding {:x {:field :timestamp
+                                                   :type :quantitative
+                                                   :scale {:domain
+                                                           [(:min timestamp-data)
+                                                            (:max timestamp-data)]}}
+                                               :y {:field {:repeat :row}
+                                                   :type :quantitative}}}}]]]])
 
 
-(oz/view! [:div "one new thing"])
+(oz/view! view-ds)
